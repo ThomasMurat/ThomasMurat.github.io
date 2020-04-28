86d5f1868037c8892b558473b22da60bc049a92c
ThomasMurat <thomasmurat1992@gmail.com>
2020-04-28 17:01:17 +0200

    changement graphique

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 var x = screen.availHeight;
-var y = screen.availWidth;
-document.getElementById("contentPage").style.minHeight = x - 200 + "px";
+var menuHeight = document.getElementById('menuNavigation').clientHeight;
+var topHeight = document.getElementById('topArea').clientHeight;
+var contentPage = document.getElementById('contentPage');
+if (x < menuHeight) {
+    contentPage.style.minHeight = menuHeight - topHeight + "px";
+}else {
+    contentPage.style.minHeight = x - topHeight + "px";
+}
